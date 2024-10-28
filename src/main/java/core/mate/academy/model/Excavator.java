2d14cb3ca037b585b124cbf2462283d074ab783d
Valeriy <valeriym250@gmail.com>
2024-10-28 11:14:08 +0000

    Find solution for this task? but don`t know is it correct

--- src/main/java/core/mate/academy/model/Excavator.java
+++ src/main/java/core/mate/academy/model/Excavator.java
@@ -1,15 +1,29 @@
 package core.mate.academy.model;
 
+import java.util.List;
+
 /**
  * Add some custom fields that could be only in Excavator
  * Do not remove no-args constructor
  */
 public class Excavator extends Machine {
+    private String engineManufacturer;
+    private int arrowLength;
+
     public Excavator() {
     }
 
     @Override
     public void doWork() {
         System.out.println("Excavator started to work");
+    }
+
+    @Override
+    public List<Machine> get() {
+        Excavator excavator1 = new Excavator();
+        Excavator excavator2 = new Excavator();
+        Excavator excavator3 = new Excavator();
+        List<Machine> excavators = List.of(excavator1, excavator2, excavator3);
+        return excavators;
     }
 }
--- src/main/java/core/mate/academy/model/Truck.java
+++ src/main/java/core/mate/academy/model/Truck.java
@@ -1,15 +1,29 @@
 package core.mate.academy.model;
 
+import java.util.List;
+
 /**
  * Add some custom fields that could be only in Truck
  * Do not remove no-field constructor
  */
 public class Truck extends Machine {
+    private int wheelsQuantity;
+    private double cargoVolume;
+
     public Truck() {
     }
 
     @Override
     public void doWork() {
         System.out.println("Truck started to work");
+    }
+
+    @Override
+    public List<Machine> get() {
+        Truck truck1 = new Truck();
+        Truck truck2 = new Truck();
+        Truck truck3 = new Truck();
+        List<Machine> trucks = List.of(truck1, truck2, truck3);
+        return trucks;
     }
 }
